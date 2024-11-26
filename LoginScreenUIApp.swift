4aca06ba6e4d66f5d3c72f97f29faeb05d803e93
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-27 02:00:54 +0800

    Update LoginScreenUIApp.swift

--- LoginScreenUIApp.swift
+++ LoginScreenUIApp.swift
@@ -1,1 +1,18 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Luis Laguardia on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
 

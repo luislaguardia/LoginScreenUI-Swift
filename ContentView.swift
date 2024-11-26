7a14b596cef61ef22f6f29b6d065760fa9b8cdb1
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-27 02:02:47 +0800

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,1 +1,87 @@
 
+//
+//  ContentView.swift
+//  LoginScreenUI
+//
+//  Created by Luis Laguardia on 11/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                    
+                }
+                
+                
+            }
+            .navigationBarHidden(true)
+        }
+    
+    }
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "Admin" {
+            if password.lowercased() == "Password" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
